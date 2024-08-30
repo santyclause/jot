01f53b742f16045dcc70bd9f6f9ecb1d79f198fd
Matthew Marchal <matthew.marchal@gmail.com>
2024-08-30 10:04:40 -0600

    init

---
+++ app/App.js
@@ -0,0 +1,19 @@
+import { router } from './router-config.js';
+const USE_ROUTER = false
+
+class App {
+
+
+
+  constructor() {
+    if (USE_ROUTER) {
+      this.router = router
+      this.router.init(this)
+    }
+  }
+
+}
+
+const app = new App()
+// @ts-ignore
+window.app = app
---
+++ app/router-config.js
@@ -0,0 +1,11 @@
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [],
+    view: /*html*/`
+    `
+  }
+])
