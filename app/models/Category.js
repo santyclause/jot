63f94c69e344db06b8940b9f0b89860cbe3ad0f9
Matthew Marchal <matthew.marchal@gmail.com>
2024-08-30 11:27:58 -0600

    set up initial object class constructors

--- app/models/Category.js
+++ app/models/Category.js
@@ -1,3 +1,8 @@
-export class Category {
+import { generateId } from "../utils/GenerateId.js"
 
+export class Category {
+  constructor(data) {
+    this.id = data.id || generateId();
+    this.name = data.name;
+  }
 }
